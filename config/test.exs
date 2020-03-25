3c740fa1dc4bcd23e36ce24793ddd99290342cf7
Gustavo Aguiar <gugahoa@gmail.com>
2020-03-25 02:19:44 -0300

    change db hostname in test environment

--- config/test.exs
+++ config/test.exs
@@ -5,7 +5,7 @@ config :bank_accounting, BankAccounting.Repo,
   username: "postgres",
   password: "postgres",
   database: "bank_accounting_test",
-  hostname: "localhost",
+  hostname: "db",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
